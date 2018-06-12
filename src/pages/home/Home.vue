f576c1eea7a1c909a586bd01ede4063641066be1
jack <qiuzt@cn.ibm.com>
2018-06-12 09:55:27 -0400

    update header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div class="home">Hello World</div>
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'home'
+  name: 'home',
+  components:{
+    HomeHeader
+  }
 }
 </script>
 
