9c26d0ec41ed14774c8378b2a7fc8f276c625d16
developerQiu <38775878+developerQiu@users.noreply.github.com>
2018-06-13 10:08:45 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">城市</div>
-    <div class="header-right">内容</div>
+    <div class="header-left">
+      <span class="iconfont icon-fanhui"></span>
+    </div>
+    <div class="header-input"><span class="iconfont icon-sousuo"></span>输入经典/城市/游玩/主题</div>
+    <div class="header-right">城市<span class="iconfont icon-jiantou"></span></div>
   </div>
 </template>
 
@@ -13,15 +15,26 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
   .header
     height: .86rem
+    line-height: .86rem
     display: flex
-    background-color: #00bcd4
+    background-color: $bgColor
     color: #fff
     .header-left
       width: .64rem
     .header-input
       flex:1
+      height .64rem
+      line-height .64rem
+      margin .12rem 0 0 .2rem
+      background-color #fff
+      border-radius .1rem
+      color #cccccc
     .header-right
       width: 1.24rem
+      text-align center
+      .icon-jiantou
+        font-size .24rem
 </style>
