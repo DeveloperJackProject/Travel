c4f04ad11a60a3265990cf4eb57e89d6dc2b2e79
Jack <535999731@qq.com>
2018-06-14 19:21:18 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-image" :src="item.imgaeUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgaeUrl: 'https://b2-q.mafengwo.net/s10/M00/4B/E6/wKgBZ1oKu-uAI6yuAAFxwUc4cgY85.jpeg'
+        },
+        {
+          id: '0002',
+          imgaeUrl: 'https://p1-q.mafengwo.net/s11/M00/39/40/wKgBEFsP1vGABq8tAAGI1yRVDos14.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color aliceblue
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 40%
+    .swiper-image
+      width 100%
+
+</style>
