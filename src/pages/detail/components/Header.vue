ab069f7447aff2523ab40738f0ab3372a35e1e88
jack <qiuzt@cn.ibm.com>
2018-06-20 02:20:35 -0400

    header animation

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,17 +1,58 @@
 <template>
   <div>
-    <div class="header-abs"></div>
-    <div class="header-fixed"></div>
+    <router-link
+      tag="div"
+      class="header-abs"
+      to="/"
+      v-show="showAbs"
+    >
+      <span class="iconfont icon-fanhui header-abs-back"></span>
+    </router-link>
+    <div
+      class="header-fixed"
+      v-show="!showAbs"
+      :style="opacityStyle"
+    >
+      <router-link to="/">
+        <div class="iconfont icon-fanhui header-fixed-back"></div>
+      </router-link>
+      景点详情
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {opacity}
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header-abs
     position absolute
     left .2rem
@@ -19,4 +60,29 @@ export default {
     width .8rem
     height .8rem
     border-radius .4rem
+    line-height .8rem
+    background rgba(0, 0, 0, .8)
+    text-align center
+    .header-abs-back
+      color #ffffff
+      font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-fixed-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #fff
 </style>
